fd45ac74687fde8e1e1a1c10b25420d8bd2b75e5
Noman Ejaz <nomanejaz132@gmail.com>
2021-01-04 21:56:26 +0500

    First Commit

---
+++ src/components/ReactIcons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BsCloud } from 'react-icons/bs';
+
+function ReactIcons() {
+    return (
+        <div>
+            <BsCloud />
+        </div>
+    )
+}
+
+export default ReactIcons
